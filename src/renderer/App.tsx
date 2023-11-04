ae799cc23e495479199134e9537a3fa294082fa4
MentalSmoge <mentalsmoge@gmail.com>
2023-11-04 18:51:32 +0500

    Init

---
+++ src/renderer/App.tsx
@@ -0,0 +1,15 @@
+import Flow from '../Flow';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">React Flow - CRA Example</header>
+      <Flow />
+      <footer className="App-footer">React Flow - CRA Example</footer>
+    </div>
+  );
+}
+
+export default App;
