d48da31f803561c190da289507fedc631edbde7a
MentalSmoge <mentalsmoge@gmail.com>
2024-02-26 23:42:07 +0500

    installed react-textarea-autosize

--- src/Flow/Components/DefaultInput/index.tsx
+++ src/Flow/Components/DefaultInput/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import TextareaAutosize from 'react-textarea-autosize';
 import "./DefaultInput.css"
 import { DefaultInputProps } from "../../types";
 
+
 // eslint-disable-next-line react/function-component-definition
 const DefaultInput = ({textContent, readOnly=false} : DefaultInputProps) => {
-  return (<textarea value={textContent} readOnly={readOnly} className='DefaultInput nodrag nowheel'/>);
+  return (<TextareaAutosize cols={30} maxRows={10} minRows={3} value={textContent} readOnly={readOnly} className='DefaultInput nodrag nowheel'/>);
 }
 
 export default DefaultInput
