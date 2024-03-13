f68869aa35c9ca196e30bc558a8a0dca81a5a2a4
MentalSmoge <mentalsmoge@gmail.com>
2024-03-13 11:05:42 +0500

    Added awful slide animation

--- src/Flow/Components/RightSideBar/index.tsx
+++ src/Flow/Components/RightSideBar/index.tsx
@@ -4,23 +4,44 @@ import "./RightSideBar.css"
 import DialogsStore from "../../../store/DialogsStore";
 import SideBarButton from "./RightSideBar-button";
 import AddDialogButton from "../AddDialogModal/AddDialogButton";
+import { CSSTransition } from "react-transition-group";
+import CharacterStore from "../../../store/CharacterStore";
+import VariablesStore from "../../../store/VariablesStore";
 
 
 // eslint-disable-next-line react/function-component-definition
 const RightSideBar = () => {
   const [open, setOpen] = useState(false)
+  const [category, setCategory] = useState(1)
   return (
     <>
-      <button type="button" onClick={() => {setOpen(!open)}} >Reveal</button>
-      {open && <div className="RightSideBar">
-          <div className="RightSideBar-wrapper">
-            {DialogsStore.dialogs.map((dialog, index) => (
-              <SideBarButton key={dialog.id} dialog={dialog} />
-            ))}
+      <button className="RightSideBar-reveal-button button_neutral" type="button" onClick={() => {setOpen(!open)}} >Reveal</button>
+      <CSSTransition
+        in={open}
+        timeout={500}
+        classNames="circle"
+        mountOnEnter
+        unmountOnExit
+      >
+
+        <div className="RightSideBar">
+          <div className="RightSideBar-buttons-row">
+            <button type="button" className= {category===1 ? "RightSideBar-selector button_neutral highlight" : "RightSideBar-selector button_neutral"} onClick={() => setCategory(1)} >Персонажи</button>
+            <button type="button" className= {category===2 ? "RightSideBar-selector button_neutral highlight" : "RightSideBar-selector button_neutral"} onClick={() => setCategory(2)}>Переменные</button>
           </div>
-          <AddDialogButton />
-      </div>
-      }
+            <div className="RightSideBar-wrapper">
+              {category === 1 && CharacterStore.characters.map((character, index) => (
+                <button type="button">{character.name}</button>
+              ))}
+              {category === 2 && VariablesStore.variables.map((variable, index) => (
+                <button type="button">{variable.label}</button>
+              ))}
+
+            </div>
+            <AddDialogButton />
+        </div>
+      </CSSTransition>
+
 
     </>
   );
