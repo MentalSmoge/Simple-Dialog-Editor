20eef4439c803f2de07a8c53c4eeae5623e34247
MentalSmoge <mentalsmoge@gmail.com>
2024-04-15 11:11:24 +0500

    Something

--- src/pages/MainEditor/Nodes/Node_Branch/NodeBranch.tsx
+++ src/pages/MainEditor/Nodes/Node_Branch/NodeBranch.tsx
@@ -6,20 +6,30 @@ import './NodeBranch.css';
 import ChoiceRow from './components/ChoiceRow';
 import FlowStore from '../../components/EditorField/FlowStore';
 
-
 // eslint-disable-next-line react/function-component-definition
 const NodeBranch: FC<NodeProps> = ({ id, data }) => {
   return (
-    <div className='container'>
+    <div className="container">
       <Handle type="target" position={Position.Left} />
-      <div className='wrapper'>
-      {data.rows.map((row, index:number) => (
-          <ChoiceRow id={id} key={row.idOfRow} idOfRow={row.idOfRow} data={row.data} position={index} renderDelete={data.rows.length > 2}/>
+      <div className="wrapper">
+        {data.rows.map((row, index: number) => (
+          <ChoiceRow
+            id={id}
+            key={row.idOfRow}
+            idOfRow={row.idOfRow}
+            data={row.data}
+            position={index}
+            renderDelete={data.rows.length > 2}
+          />
         ))}
       </div>
-        <button type="button" className='buttonInChoice button_neutral nodrag' onClick={() => FlowStore.addRow(id)}>Добавить условие</button>
-        {/* <button type="button" className='buttonInChoice button_neutral' onClick={testButton}>Тестовая</button> */}
-
+      <button
+        type="button"
+        className="buttonInChoice button_neutral nodrag"
+        onClick={() => FlowStore.addRow(id)}
+      >
+        Добавить условие
+      </button>
     </div>
   );
 };
